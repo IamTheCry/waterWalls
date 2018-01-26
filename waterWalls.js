e9407d84662d0df7b0e8e8ec822ee906995507c4
Corey <cry@Coreys-MacBook-Air.local>
2018-01-25 16:51:47 -0800

    Create helper function stubs in waterWalls

--- waterWalls.js
+++ waterWalls.js
@@ -1,5 +1,16 @@
 const waterWalls = (arr) => {
+  let biggestBlock = [];
+  // loop through 
   // return block with most water
+  return biggestBlock;
+}
+
+const findBlock = (arr) => {
+  // returns an array of arrays, showing start and end indexes of blocks
+}
+
+const countWater = (arr) => {
+  // takes an array of blocks and calculates how much water is in each
 }
 
 module.exports = waterWalls;
